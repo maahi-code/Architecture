4899caa5b6caa583f34430652520549b24c7f82f
Mahipal Singh <ms4341547@gmail.com>
2026-07-06 20:44:52 +0530

    Initial Commit

---
+++ Architecture/ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureApp.swift
+//  Architecture
+//
+//  Created by Mahipal Singh on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
