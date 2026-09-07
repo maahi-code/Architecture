a94c49d841cf48c6fe1ea62f77e6c49dab3ff55e
Mahipal Singh <ms4341547@gmail.com>
2026-09-07 20:42:51 +0530

    [Feature] Support Child RouterViews

--- Architecture/Core/ProfileView.swift
+++ Architecture/Core/ProfileView.swift
@@ -30,54 +30,201 @@ struct AnyDestination: Hashable {
 }
 
 protocol Router {
-     func showScreen<T: View>(@ViewBuilder destination: () -> T)
+    func showScreen<T: View>(@ViewBuilder destination: @escaping (Router) -> T)
+    func dismissScreen()
 }
 struct RouterView<Content: View> : View, Router {
     @State private var path: [AnyDestination] = []
+    // @Binding to the view stack from previous RouteViews
+    @Binding var screenStack: [AnyDestination]
+    var addNavigationView: Bool
     @ViewBuilder var content: (Router) -> Content
-    var body: some View {
-        NavigationStack(path: $path) {
-            content(self)
-                .navigationDestination(for: AnyDestination.self) { value in
-                    value.destination
-                }
-            
-        }
+    @Environment(\.dismiss) private var dismiss
+    
+    init(
+        screenStack: (Binding<[AnyDestination]>)? = nil,
+        addNavigationView: Bool = true,
+        content: @escaping (Router) -> Content,
+    ) {
+        self._screenStack = screenStack ?? .constant([])
+        self.addNavigationView = addNavigationView
+        self.content = content
     }
-    func showScreen<T: View>(@ViewBuilder destination: () -> T) {
-        let destination = AnyDestination(destination: destination())
-        path.append(destination)
+    var body: some View {
+        NavigationStackIfNeeded(
+            path: $path,
+            addNavigationView: addNavigationView) {
+                content(self)
+            }
+    }
+    func showScreen<T: View>(@ViewBuilder destination: @escaping (Router) -> T) {
+        let screen = RouterView<T>(
+            screenStack: screenStack.isEmpty ? $path : $screenStack,
+            addNavigationView: false) { newRouter in
+                destination(newRouter)
+            }
+        let destination = AnyDestination(destination: screen)
+        if screenStack.isEmpty {
+            // This means we are on the main RouterView
+            path.append(destination)
+        } else {
+            // This means we are on the secondary RouterView
+            screenStack.append(destination)
+        }
+        
+    }
+    
+    func dismissScreen() {
+        dismiss()
+    }
+}
+
+/*
+  RouterView - @Environment
+     ProfileView
+       RouterView - @Environment
+          SettingsView
+            RouterView - @Environment
+               AccountView
+ 
+ 
+ 
+ 
+ */
+
+struct NavigationStackIfNeeded<Content: View> : View {
+    @Binding var path: [AnyDestination]
+    var addNavigationView: Bool = true
+    @ViewBuilder var content: Content
+    var body: some View {
+        if addNavigationView {
+            NavigationStack(path: $path) {
+                content
+                    .navigationDestination(for: AnyDestination.self) { value in
+                        value.destination
+                    }
+            }
+        } else {
+            content
+        }
     }
 }
 struct ProfileView: View {
     @State private var path: [AnyDestination] = []
+    let router: Router
     var body: some View {
-        RouterView { router in
-            VStack(spacing: 50) {
-                Button {
-                    router.showScreen {
-                        ZStack {
-                            Color.blue.ignoresSafeArea()
-                            Text("NEW SCREEN HORRAY!!!")
-                                .font(.title)
-                                .foregroundStyle(.white)
-                                .bold()
-                        }
-                    }
-                } label: {
-                    Text("Click me")
+        VStack(spacing: 50) {
+            Button {
+                router.showScreen { router in
+                    SettingsView(router: router)
                 }
+            } label: {
+                Text("Go to the Settings")
             }
-                
         }
     }
 }
 
 #Preview {
-    ProfileView()
+    RouterView { router in
+        ProfileView(router: router)
+    }
 }
 
-
+struct SettingsView: View {
+    let router: Router
+    var body: some View {
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            VStack{
+                Text("Settings")
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                Spacer()
+                HStack {
+                    
+                    Button {
+                        router.dismissScreen()
+                    } label: {
+                        Text("Previous")
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 5)
+                            
+                    }
+                    .buttonStyle(.glass)
+                    .padding(.horizontal)
+                    
+                    
+                    Button {
+                        router.showScreen { router in
+                            AccountView(router: router)
+                        }
+                    } label: {
+                        Text("Account Screen")
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 5)
+                            
+                    }
+                    .buttonStyle(.glass)
+                    .padding(.horizontal)
+                }
+                
+                Spacer()
+            }
+            
+        }
+    }
+}
+struct AccountView: View {
+    let router: Router
+    var body: some View {
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            VStack{
+                Text("Account")
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                Spacer()
+                HStack {
+                    
+                    Button {
+                        router.dismissScreen()
+                    } label: {
+                        Text("Previous")
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 5)
+                            
+                    }
+                    .buttonStyle(.glass)
+                    .padding(.horizontal)
+                    
+                    
+                    Button {
+                        router.showScreen { router in
+                            AccountView(router: router)
+                        }
+                    } label: {
+                        Text("Account Screen")
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 5)
+                            
+                    }
+                    .buttonStyle(.glass)
+                    .padding(.horizontal)
+                }
+                
+                Spacer()
+            }
+            
+        }
+    }
+}
 extension View {
     func any() -> AnyView {
         AnyView(self)
