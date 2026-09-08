94b6ab81c7dcea62ef4a4679eb973ce942dfc061
Mahipal Singh <ms4341547@gmail.com>
2026-09-08 20:37:26 +0530

    [feature] Add Segue Option(sheet, fullScreenCover)

--- Architecture/Core/ProfileView.swift
+++ Architecture/Core/ProfileView.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
-enum NavigationDestinationOption: Hashable {
-    case intergerScreen(int: Int)
-    case stringScreen(str: String)
-    case someOtherScreen(bool: Bool)
-}
+//enum NavigationDestinationOption: Hashable {
+//    case intergerScreen(int: Int)
+//    case stringScreen(str: String)
+//    case someOtherScreen(bool: Bool)
+//}
 
+extension Binding where Value == Bool {
+    init<T: Sendable>(ifNotNil value: Binding<T?>) {
+        self.init {
+            value.wrappedValue != nil
+        } set: { newValue in
+            if !newValue {
+                value.wrappedValue = nil
+            }
+        }
+
+    }
+}
 
 struct AnyDestination: Hashable {
     let id = UUID().uuidString
@@ -35,7 +47,7 @@ extension EnvironmentValues {
 
 
 struct MockRouter: Router {
-    func showScreen<T>(destination: @escaping (any Router) -> T) where T : View {
+    func showScreen<T: View>(_ segue: SegueOption, @ViewBuilder destination: @escaping (Router) -> T) where T : View {
         print("Mock Router don't work!!!")
     }
     func dismissScreen() {
@@ -44,7 +56,7 @@ struct MockRouter: Router {
 }
 
 protocol Router {
-    func showScreen<T: View>(@ViewBuilder destination: @escaping (Router) -> T)
+    func showScreen<T: View>(_ segue: SegueOption, @ViewBuilder destination: @escaping (Router) -> T)
     func dismissScreen()
 }
 
@@ -52,6 +64,10 @@ protocol Router {
 
 struct RouterView<Content: View> : View, Router {
     @State private var path: [AnyDestination] = []
+    
+    
+    @State private var showSheet: AnyDestination? = nil
+    @State private var showFullScreenCover: AnyDestination? = nil
     
     // @Binding to the view stack from previous RouteViews
     @Binding var screenStack: [AnyDestination]
@@ -76,34 +92,54 @@ struct RouterView<Content: View> : View, Router {
     var body: some View {
         NavigationStackIfNeeded(path: $path, addNavigationView: addNavigationView) {
             content(self)
+                .sheetViewModifier(screen: $showSheet)
+                .fullScreenCoverViewModifier(screen: $showFullScreenCover)
         }
         .environment(\.router, self)
     }
     
     
     
-    func showScreen<T: View>(@ViewBuilder destination: @escaping (Router) -> T) {
+    func showScreen<T: View>(_ option: SegueOption, @ViewBuilder destination: @escaping (Router) -> T) {
         let screen = RouterView<T>(
             screenStack: screenStack.isEmpty ? $path : $screenStack,
-            addNavigationView: false) { newRouter in
+            addNavigationView: option.shouldAddNewNavigationView) { newRouter in
                 destination(newRouter)
             }
         let destination = AnyDestination(destination: screen)
-        if screenStack.isEmpty {
-            // This means we are on the main RouterView
-            path.append(destination)
-        } else {
-            // This means we are on the secondary RouterView
-            screenStack.append(destination)
+        switch option {
+        case .push:
+            if screenStack.isEmpty {
+                // This means we are on the main RouterView
+                path.append(destination)
+            } else {
+                // This means we are on the secondary RouterView
+                screenStack.append(destination)
+            }
+        case .sheet:
+            showSheet = destination
+        case .fullScreenCover:
+            showFullScreenCover = destination
         }
-        
     }
     
     func dismissScreen() {
         dismiss()
     }
 }
-
+ 
+enum SegueOption {
+    case push, sheet, fullScreenCover
+    
+    var shouldAddNewNavigationView: Bool {
+        switch self {
+        case .push:
+            return false
+        case .sheet, .fullScreenCover:
+            return true
+        }
+    }
+}
 /*
   RouterView - @Environment
      ProfileView
@@ -140,7 +176,7 @@ struct ProfileView: View {
     var body: some View {
         VStack(spacing: 50) {
             Button {
-                router.showScreen { _ in
+                router.showScreen(.fullScreenCover) { _ in
                     SettingsView()
                 }
             } label: {
@@ -178,7 +214,7 @@ struct SettingsView: View {
                     
                     
                     Button {
-                        router.showScreen { _ in
+                        router.showScreen(.push) { _ in
                             AccountView()
                         }
                     } label: {
@@ -225,7 +261,7 @@ struct AccountView: View {
                     
                     
                     Button {
-                        router.showScreen { _ in
+                        router.showScreen(.sheet) { _ in
                             AccountView()
                         }
                     } label: {
@@ -255,5 +291,30 @@ struct AccountView: View {
 extension View {
     func any() -> AnyView {
         AnyView(self)
+    }
+}
+
+extension View {
+    func sheetViewModifier(screen:  Binding<AnyDestination?>) -> some View {
+        self
+            .sheet(isPresented: Binding(ifNotNil: screen)) {
+                ZStack {
+                    if let screen = screen.wrappedValue {
+                        screen.destination
+                    }
+                }
+            }
+        
+    }
+    func fullScreenCoverViewModifier(screen:  Binding<AnyDestination?>) -> some View {
+        self
+            .fullScreenCover(isPresented: Binding(ifNotNil: screen)) {
+                ZStack {
+                    if let screen = screen.wrappedValue {
+                        screen.destination
+                    }
+                }
+            }
+        
     }
 }
